8f3ec5478eac1ff7aa32c384117bf042410d42f1
dongdongunique <909580378@qq.com>
2024-11-30 02:07:33 +0800

    Bug Fixed

---
+++ token_generator/generate.py
@@ -0,0 +1,295 @@
+# Modified from:
+#   gpt-fast: https://github.com/pytorch-labs/gpt-fast/blob/main/generate.py
+#   DiT:      https://github.com/facebookresearch/DiT/blob/main/models.py
+import torch
+import torch.nn as nn
+import torchvision.transforms.functional
+from torch.nn import functional as F
+import torch._dynamo.config
+import torch._inductor.config
+import copy
+from einops import rearrange
+
+
+# torch._inductor.config.coordinate_descent_tuning = True
+# torch._inductor.config.triton.unique_kernel_names = True
+# torch._inductor.config.fx_graph_cache = True # Experimental feature to reduce compilation times, will be on by default in future
+
+
+# from https://huggingface.co/transformers/v3.2.0/_modules/transformers/generation_utils.html
+def top_k_top_p_filtering(
+        logits,
+        top_k: int = 0,
+        top_p: float = 1.0,
+        filter_value: float = -float("Inf"),
+        min_tokens_to_keep: int = 1
+):
+    """Filter a distribution of logits using top-k and/or nucleus (top-p) filtering
+    Args:
+        logits: logits distribution shape (batch size, vocabulary size)
+        if top_k > 0: keep only top k tokens with highest probability (top-k filtering).
+        if top_p < 1.0: keep the top tokens with cumulative probability >= top_p (nucleus filtering).
+            Nucleus filtering is described in Holtzman et al. (http://arxiv.org/abs/1904.09751)
+        Make sure we keep at least min_tokens_to_keep per batch example in the output
+    From: https://gist.github.com/thomwolf/1a5a29f6962089e871b94cbd09daf317
+    """
+    if top_k > 0:
+        top_k = min(max(top_k, min_tokens_to_keep), logits.size(-1))  # Safety check
+        # Remove all tokens with a probability less than the last token of the top-k
+        indices_to_remove = logits < torch.topk(logits, top_k)[0][..., -1, None]
+        logits[indices_to_remove] = filter_value
+
+    if top_p < 1.0:
+        sorted_logits, sorted_indices = torch.sort(logits, descending=True)
+        cumulative_probs = torch.cumsum(F.softmax(sorted_logits, dim=-1), dim=-1)
+
+        # Remove tokens with cumulative probability above the threshold (token with 0 are kept)
+        sorted_indices_to_remove = cumulative_probs > top_p
+        if min_tokens_to_keep > 1:
+            # Keep at least min_tokens_to_keep (set to min_tokens_to_keep-1 because we add the first one below)
+            sorted_indices_to_remove[..., :min_tokens_to_keep] = 0
+        # Shift the indices to the right to keep also the first token above the threshold
+        sorted_indices_to_remove[..., 1:] = sorted_indices_to_remove[..., :-1].clone()
+        sorted_indices_to_remove[..., 0] = 0
+
+        # scatter sorted tensors to original indexing
+        indices_to_remove = sorted_indices_to_remove.scatter(1, sorted_indices, sorted_indices_to_remove)
+        logits[indices_to_remove] = filter_value
+    return logits
+
+
+def sample(
+        logits,
+        temperature: float = 1.0,
+        top_k: int = 0,
+        top_p: float = 1.0,
+        sample_logits=True
+):
+    logits = logits[:, -1, :] / max(temperature, 1e-5)
+    if top_k > 0 or top_p < 1.0:
+        logits = top_k_top_p_filtering(logits, top_k=top_k, top_p=top_p)
+    probs = F.softmax(logits, dim=-1)
+    if sample_logits:
+        idx = torch.multinomial(probs, num_samples=1)
+    else:
+        _, idx = torch.topk(probs, k=1, dim=-1)
+    return idx, probs
+
+
+def logits_to_probs(
+        logits,
+        temperature: float = 1.0,
+        top_p: float = 1.0,
+        top_k: int = None,
+        **kwargs
+):
+    logits = logits / max(temperature, 1e-5)
+    if top_k > 0 or top_p < 1.0:
+        logits = top_k_top_p_filtering(logits, top_k=top_k, top_p=top_p)
+    probs = torch.nn.functional.softmax(logits, dim=-1)
+    return probs
+
+
+def prefill(
+        model,
+        cond_idx: torch.Tensor,
+        input_pos: torch.Tensor,
+        cfg_scale: float,
+        **sampling_kwargs
+):
+    if cfg_scale > 1.0:
+        logits, _ = model(None, cond_idx, input_pos)
+        logits_combined = logits
+        cond_logits, uncond_logits = torch.split(logits_combined, len(logits_combined) // 2, dim=0)
+        logits = uncond_logits + (cond_logits - uncond_logits) * cfg_scale
+    else:
+        logits, _ = model(None, cond_idx, input_pos)
+    return sample(logits, **sampling_kwargs)[0]
+
+
+def decode_one_token(
+        model,
+        x: torch.Tensor,
+        input_pos: torch.Tensor,
+        cfg_scale: float,
+        cfg_flag: bool,
+        **sampling_kwargs
+):
+    assert input_pos.shape[-1] == 1
+    if cfg_scale > 1.0:
+        x_combined = torch.cat([x, x])
+        logits, _ = model(x_combined, cond_idx=None, input_pos=input_pos)
+        logits_combined = logits
+        cond_logits, uncond_logits = torch.split(logits_combined, len(logits_combined) // 2, dim=0)
+        if cfg_flag:
+            logits = uncond_logits + (cond_logits - uncond_logits) * cfg_scale
+        else:
+            logits = cond_logits
+    else:
+        logits, _ = model(x, cond_idx=None, input_pos=input_pos)
+    return sample(logits, **sampling_kwargs)
+
+
+def decode_n_tokens(
+        model,
+        cur_token: torch.Tensor,
+        input_pos: torch.Tensor,
+        num_tokens: int,
+        cfg_scale: float,
+        cfg_interval: int,
+        **sampling_kwargs
+):
+    new_tokens, new_probs = [], []
+    cfg_flag = True
+    for i in range(num_tokens):
+        with torch.backends.cuda.sdp_kernel(enable_flash=False, enable_mem_efficient=False, enable_math=True):  # actually better for Inductor to codegen attention here
+            if -1 < cfg_interval < i:
+                cfg_flag = False
+            next_token, next_prob = decode_one_token(model, cur_token, input_pos, cfg_scale, cfg_flag, **sampling_kwargs)
+            input_pos += 1
+            new_tokens.append(next_token.clone())
+            new_probs.append(next_prob.clone())
+            cur_token = next_token.view(-1, 1)
+    return new_tokens, new_probs
+
+
+@torch.no_grad()
+def generate(
+        model,
+        cond: torch.Tensor, num_tokens, emb_masks: torch.Tensor = None,
+        cfg_scale=1.0, cfg_interval=-1,
+        gen_mode: str = 'h', times=3, addit_rows=16, addit_cols=16,
+        lam=1.0,
+        **sampling_kwargs
+):
+    if model.model_type != 't2i':
+        raise Exception("Please check model type!")
+
+    batch_size = cond.shape[0]
+    device = cond.device
+
+    # generate first token block
+    if cfg_scale > 1.0:
+        cond_null = torch.zeros_like(cond) + model.cls_embedding.uncond_embedding
+        cond_combined = torch.cat([cond, cond_null])
+    else:
+        cond_combined = cond
+    T = cond.shape[1]
+
+    with torch.device(device):
+        batch_size_cfg = batch_size * 2 if cfg_scale > 1.0 else batch_size
+        model.setup_caches(max_batch_size=batch_size_cfg, max_seq_length=T + num_tokens, dtype=model.tok_embeddings.weight.dtype)
+
+    if emb_masks is not None:
+        assert emb_masks.shape[0] == batch_size
+        assert emb_masks.shape[-1] == T
+        if cfg_scale > 1.0:
+            model.causal_mask[:, :, :T] = model.causal_mask[:, :, :T] * torch.cat([emb_masks, emb_masks]).unsqueeze(1)
+        else:
+            model.causal_mask[:, :, :T] = model.causal_mask[:, :, :T] * emb_masks.unsqueeze(1)
+        eye_matrix = torch.eye(model.causal_mask.size(1), model.causal_mask.size(2), device=device)
+        model.causal_mask[:] = model.causal_mask * (1 - eye_matrix) + eye_matrix
+
+    seq = torch.empty((batch_size, T + num_tokens), dtype=torch.int, device=device)  # create an empty tensor of the expected final shape and fill in the current tokens
+
+    input_pos = torch.arange(0, T, device=device)
+    cur_token = prefill(model, cond_combined, input_pos, cfg_scale, **sampling_kwargs)
+    seq[:, T:T + 1] = cur_token
+
+    input_pos = torch.tensor([T], device=device, dtype=torch.int)
+    generated_tokens, _ = decode_n_tokens(model, cur_token, input_pos, num_tokens - 1, cfg_scale, cfg_interval, **sampling_kwargs)
+    seq[:, T + 1:] = torch.cat(generated_tokens, dim=1)
+
+    # generate additional tokens, supporting vertical, horizontal, and both directions
+    seqs = [seq[:, T:]]
+    if gen_mode == 'v':
+        pano_seq = seqs[-1]
+        addit_num_tokens = addit_rows * 32
+        for t in range(times):
+            addit_seq = generate_additional_tokens_vertical(model, num_tokens, cfg_scale, cfg_interval, sampling_kwargs,
+                                                            T, device, generated_tokens=seqs[-1],
+                                                            addit_num_tokens=addit_num_tokens)
+            seqs.append(torch.cat([seqs[-1][:, addit_num_tokens:], addit_seq], dim=1))
+            pano_seq = torch.cat([pano_seq, addit_seq], dim=1)
+    elif gen_mode == 'h':
+        pano_seq = rearrange(seqs[-1], "b (h w) -> b h w", h=32)
+        for t in range(times):
+            addit_seq = generate_additional_tokens_horizontal(model, cfg_scale, sampling_kwargs, T, device,
+                                                              generated_tokens=seqs[-1], addit_cols=addit_cols)
+            seqs.append(addit_seq)
+            addit_seq = rearrange(addit_seq, "b (h w) -> b h w", h=32)
+            pano_seq = torch.cat([pano_seq, addit_seq[:, :, 32 - addit_cols:]], dim=2)
+    else:
+        times_v = times_h = times
+        pano_seq = []
+        addit_num_tokens = addit_rows * 32
+        for t_v in range(times_v + 1):
+            pano_seq_h = rearrange(seqs[-1], "b (h w) -> b h w", h=32)
+            for t_h in range(times_h):
+                addit_seq = generate_additional_tokens_horizontal(model, cfg_scale, sampling_kwargs, T, device,
+                                                                  generated_tokens=seqs[-1], addit_cols=addit_cols)
+                seqs.append(addit_seq)
+                addit_seq = rearrange(addit_seq, "b (h w) -> b h w", h=32)
+                pano_seq_h = torch.cat([pano_seq_h, addit_seq[:, :, 32 - addit_cols:]], dim=2)
+            pano_seq_h = pano_seq_h if t_v == 0 else pano_seq_h[:, 32 - addit_rows:, :]
+            pano_seq.append(pano_seq_h)
+            addit_seq = generate_additional_tokens_vertical(model, num_tokens, cfg_scale, cfg_interval, sampling_kwargs,
+                                                            T, device, generated_tokens=seqs[t_v * (times_h + 1)],
+                                                            addit_num_tokens=addit_num_tokens)
+            seqs.append(torch.cat([seqs[-1][:, addit_num_tokens:], addit_seq], dim=1))
+        pano_seq = torch.cat(pano_seq, dim=1)
+    return pano_seq, seqs
+
+
+def generate_additional_tokens_vertical(
+        model,
+        num_tokens,
+        cfg_scale, cfg_interval, sampling_kwargs, T, device,
+        generated_tokens: torch.Tensor, addit_num_tokens: int
+):
+    cur_token = generated_tokens[:, -1].unsqueeze(1)  # the last one of generated tokens
+    input_pos = torch.tensor([T + num_tokens - addit_num_tokens], device=device, dtype=torch.int)
+    addit_tokens, _ = decode_n_tokens(model, cur_token, input_pos, addit_num_tokens, cfg_scale, cfg_interval, **sampling_kwargs)
+    addit_seq = torch.cat(addit_tokens, dim=1)
+    return addit_seq
+
+
+def generate_additional_tokens_horizontal(
+        model,
+        cfg_scale, sampling_kwargs, T, device,
+        generated_tokens: torch.Tensor, addit_cols: int
+):
+    generated_tokens = rearrange(generated_tokens, "b (h w) -> b h w", h=32)
+    updated_tokens = torch.randint(0, 1, generated_tokens.shape, device=device) - 1
+    updated_tokens[:, :, :32 - addit_cols] = generated_tokens[:, :, addit_cols:]
+    updated_tokens = rearrange(updated_tokens, "b h w -> b (h w)", h=32)
+    cur_token = updated_tokens[:, 31 - addit_cols].unsqueeze(1)  # the last token of each row of generated tokens
+    for i in range(32 - addit_cols, 1024):
+        input_pos = torch.tensor([T + i - 1], device=device, dtype=torch.int)
+        next_token, _ = decode_one_token(model, cur_token, input_pos, cfg_scale, True, **sampling_kwargs)
+        if torch.all(updated_tokens[:, i] == -1):
+            updated_tokens[:, i] = next_token.squeeze(1)
+        # else:
+        #     updated_tokens[:, i] = average_tokens(vq_model, updated_tokens[:, i], next_token.squeeze(1), lam=0.8)  # lam=1.0 achieves the best performance, equivalent to commenting out this line
+        cur_token = updated_tokens[:, i].unsqueeze(1)
+    return updated_tokens
+
+
+def average_tokens(vq_model, token1, token2, lam: float):
+    if vq_model.l2_norm:
+        embedding = F.normalize(vq_model.embedding.weight, p=2, dim=-1)
+    embedding1, embedding2 = embedding[token1], embedding[token2]
+    ave_embedding = lam * embedding1 + (1 - lam) * embedding2
+    d = torch.sum(ave_embedding ** 2, dim=1, keepdim=True) + \
+        torch.sum(embedding ** 2, dim=1) - 2 * \
+        torch.einsum('bd,dn->bn', ave_embedding, torch.einsum('n d -> d n', embedding))
+    ave_tokens = torch.argmin(d, dim=1)
+    return ave_tokens
+
+
+def rotater(vq_model, qzshape, seq):
+    samples = vq_model.decode_code(seq, qzshape)
+    rotated_samples = torch.rot90(samples, -1, [2, 3])  # rotate image samples 90° clockwise
+    rotated_tokens = vq_model.encode_code(rotated_samples)
+    rotated_tokens = rearrange(rotated_tokens, "b h w-> b (h w)")
+    return rotated_tokens
---
+++ token_generator/sample.py
@@ -0,0 +1,163 @@
+import torch
+torch.backends.cuda.matmul.allow_tf32 = True
+torch.backends.cudnn.allow_tf32 = True
+torch.set_float32_matmul_precision('high')
+setattr(torch.nn.Linear, 'reset_parameters', lambda self: None)  # disable default parameter init for faster speed
+setattr(torch.nn.LayerNorm, 'reset_parameters', lambda self: None)  # disable default parameter init for faster speed
+from torchvision.utils import save_image
+
+import os
+import argparse
+from image_tokenizer.vq_model import VQ_models
+from text_encoder.t5 import T5Embedder
+from token_generator.gpt import GPT_models
+from token_generator.generate import generate
+
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+
+# a naive way to implement left-padding
+def process_left_padding(caption_embs, emb_masks):
+    new_emb_masks = torch.flip(emb_masks, dims=[-1])
+    new_caption_embs = []
+    for i, (caption_emb, emb_mask) in enumerate(zip(caption_embs, emb_masks)):
+        valid_num = int(emb_mask.sum().item())
+        print(f"  prompt {i} token len: {valid_num}")
+        new_caption_emb = torch.cat([caption_emb[valid_num:], caption_emb[:valid_num]])
+        new_caption_embs.append(new_caption_emb)
+    new_caption_embs = torch.stack(new_caption_embs)
+    return new_caption_embs, new_emb_masks
+
+
+def main(args):
+    # setup pytorch
+    if args.seed != -1:
+        torch.manual_seed(args.seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+    torch.set_grad_enabled(False)
+    device = 'cuda:1'
+
+    # load vq model
+    print("Loading vq model...")
+    vq_model = VQ_models[args.vq_model](
+        codebook_size=args.codebook_size,
+        codebook_embed_dim=args.codebook_embed_dim)
+    vq_model.to(device)
+    vq_model.eval()
+    checkpoint = torch.load(args.vq_ckpt, map_location="cpu")
+    vq_model.load_state_dict(checkpoint['model'])
+    del checkpoint
+
+    # load gpt model
+    print("Loading gpt model...")
+    precision = {'none': torch.float32, 'bf16': torch.bfloat16, 'fp16': torch.float16}[args.precision]
+    latent_size = args.image_size // args.downsample_size
+    gpt_model = GPT_models[args.gpt_model](
+        block_size=latent_size ** 2,
+        cls_token_num=args.cls_token_num,
+        model_type='t2i',
+    ).to(device=device, dtype=precision)
+    checkpoint = torch.load(args.gpt_ckpt, map_location="cpu")
+    if "model" in checkpoint:  # ddp
+        model_weight = checkpoint['model']
+    elif "module" in checkpoint:  # deepspeed
+        model_weight = checkpoint['module']
+    elif "state_dict" in checkpoint:
+        model_weight = checkpoint['state_dict']
+    else:
+        raise Exception("please check model weight")
+    gpt_model.load_state_dict(model_weight, strict=False)
+    gpt_model.eval()
+    del checkpoint
+
+    # compile gpt model if needed
+    if args.compile:
+        print("Compiling gpt model...")
+        # requires PyTorch 2.0 (optional)
+        gpt_model = torch.compile(
+            gpt_model,
+            mode='reduce-overhead',
+            fullgraph=True
+        )
+
+    # load t5 model
+    print("Loading t5 model...")
+    t5_model = T5Embedder(
+        device=device,
+        local_cache=True,
+        cache_dir=args.t5_path,
+        dir_or_name=args.t5_model_type,
+        torch_dtype=precision,
+        model_max_length=args.t5_feature_max_len,
+    )
+
+    # prompts -> conditional embeddings and masks
+    caption_embs, emb_masks = t5_model.get_text_embeddings(args.prompts)
+    if not args.no_left_padding:
+        print("Processing left-padding for prompts...")
+        caption_embs, emb_masks = process_left_padding(caption_embs, emb_masks)
+    cond_tokens = caption_embs * emb_masks[:, :, None]
+
+    # generate tokens
+    print("Generating tokens...")
+    pano_tokens, block_tokens = generate(
+        model=gpt_model,
+        cond=cond_tokens, num_tokens=latent_size ** 2, emb_masks=emb_masks,
+        cfg_scale=args.cfg_scale,
+        gen_mode=args.gen_mode, times=args.times, addit_rows=args.addit_rows, addit_cols=args.addit_cols,
+        temperature=args.temperature, top_k=args.top_k,
+        top_p=args.top_p, sample_logits=True,
+    )
+
+    # tokens -> panoramas
+    print("Decoding tokens into images...")
+    addit_latent_size_col = 0 if args.gen_mode == 'v' else args.times * args.addit_cols
+    addit_latent_size_row = 0 if args.gen_mode == 'h' else args.times * args.addit_rows
+    pano_qzshape = [len(cond_tokens), args.codebook_embed_dim, latent_size + addit_latent_size_row, latent_size + addit_latent_size_col]  # [n,8,32+addit_row,32+addit_col]
+    pano_samples = vq_model.decode_code(pano_tokens, pano_qzshape)
+
+    # save panoramas
+    save_image(pano_samples, f"output.png", nrow=1, normalize=True, value_range=(-1, 1), padding=0)
+
+    # if record blocks
+    qzshape = [len(cond_tokens), args.codebook_embed_dim, latent_size, latent_size]  # [n,8,32,32]
+    for i, block_tokens_i in enumerate(block_tokens):
+        block_samples = vq_model.decode_code(block_tokens_i, qzshape)
+        save_image(block_samples, f"output_block{i}.png", nrow=4, normalize=True, value_range=(-1, 1), padding=0)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--t5-path", type=str, default="text_encoder/models")
+    parser.add_argument("--t5-model-type", type=str, default='flan-t5-xl')
+    parser.add_argument("--t5-feature-max-len", type=int, default=120)
+    parser.add_argument("--t5-feature-dim", type=int, default=2048)
+    parser.add_argument("--no-left-padding", action='store_true', default=False)
+    parser.add_argument("--gpt-model", type=str, choices=list(GPT_models.keys()), default="GPT-XL")
+    parser.add_argument("--gpt-ckpt", type=str, default="token_generator/models/t2i_XL_stage2_512.pt")
+    parser.add_argument("--cls-token-num", type=int, default=120, help="max token number of condition input")
+    parser.add_argument("--precision", type=str, default='bf16', choices=["none", "fp16", "bf16"])
+    parser.add_argument("--compile", action='store_true', default=False)
+    parser.add_argument("--vq-model", type=str, choices=list(VQ_models.keys()), default="VQ-16")
+    parser.add_argument("--vq-ckpt", type=str, default="image_tokenizer/models/vq_ds16_t2i.pt", help="ckpt path for vq model")
+    parser.add_argument("--codebook-size", type=int, default=16384, help="codebook size for vector quantization")
+    parser.add_argument("--codebook-embed-dim", type=int, default=8, help="codebook dimension for vector quantization")
+    parser.add_argument("--image-size", type=int, choices=[256, 384, 512], default=512)
+    parser.add_argument("--downsample-size", type=int, choices=[8, 16], default=16)
+    parser.add_argument("--cfg-scale", type=float, default=7.5)
+    parser.add_argument("--seed", type=int, default=-1)
+    parser.add_argument("--top-k", type=int, default=1000, help="top-k value to sample with")
+    parser.add_argument("--temperature", type=float, default=1.0, help="temperature value to sample with")
+    parser.add_argument("--top-p", type=float, default=1.0, help="top-p value to sample with")
+    parser.add_argument("--prompts", type=str, default="Generate a sprawling, verdant countryside landscape with rolling hills, winding streams, and distant mountains. The scene should have a classic, pastoral aesthetic, with lush green meadows dotted with vibrant wildflowers and clusters of deciduous trees. A small, rustic village with stone buildings and thatched roofs should sit nestled in a valley, surrounded by neatly tended farmland and orchards. A dirt road should wind its way through the landscape, flanked by picket fences and the occasional weathered barn or silo. The sky should be a warm, hazy blue, with fluffy white clouds drifting lazily overhead. The overall mood should be one of tranquility, timelessness, and the natural beauty of the countryside. The color palette should be muted and earthy, with plenty of greens, browns, grays, and ochres. The lighting should be soft and diffused, creating long shadows and highlights that give depth and dimensionality to the scene. Attention to detail is important, so include small touches like wildflowers, grazing livestock, and the smoke rising from chimneys in the village. This landscape should have a distinctly European, almost storybook-like quality, evoking the pastoral landscapes of Normandy, the English countryside, or the rolling hills of Tuscany. The entire scene should feel cohesive and harmonious, with a sense of balance and proportion that makes it feel like a complete, self-contained world.")
+    parser.add_argument("--times", type=int, default=12, help="the number of times additional tokens are generated")
+    parser.add_argument("--addit-rows", type=int, default=16, help="additional token rows generated each time")  # limit:[0,31]
+    parser.add_argument("--addit-cols", type=int, default=24, help="additional token columns generated each time")  # limit:[0,31], recommend:[16,30]
+    parser.add_argument("--lam", type=float, default=1.0, help="average factor of token embeddings")
+    parser.add_argument('--gen-mode', type=str, choices=['h', 'v', 'both'], default='h', help="the orientation of generation, horizontal, vertical, or both")
+    parser.add_argument('--n', type=int, default=1, help="number of panoramas to generate")
+    args = parser.parse_args()
+    args.prompts = [args.prompts] * args.n
+    main(args)
+
